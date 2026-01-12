5c237bb2170bfaa4b7baf84547bf186445b1f84d
Yasmine JOMAA <yasmine.jomaa@etu.u-pec.fr>
2026-01-12 16:23:10 +0100

    Premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
 
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
+    return jsonify({"message": "Hello World"})
+
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
+@app.route('/api/utilisateurs', methods=['POST'])
+def creer_utilisateur():
+    data = request.get_json()
+    nom = data.get("nom")
+    return jsonify({"nom": nom})
 
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
