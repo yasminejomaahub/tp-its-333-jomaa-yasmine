485553a921870c9bdb4db065b6a1d98988596fd6
Yasmine JOMAA <yasmine.jomaa@etu.u-pec.fr>
2026-01-13 11:27:49 +0100

    exo patient done

---
+++ srcCodeFichiersSemiStruct/search.py
@@ -0,0 +1,18 @@
+import json
+
+def search_patient_by_id(patient_id):
+    
+    fichier_json = 'BDD101/patients.json'  
+    with open(fichier_json, 'r') as f:
+        patients = json.load(f)
+
+    for patient in patients:
+        if patient['id'] == patient_id:
+            return json.dumps(patient, ensure_ascii=False, indent=4)
+
+    return json.dumps({"error": f"Patient avec id {patient_id} non trouvé"}, ensure_ascii=False, indent=4)
+
+
+id_cherche = 1
+fiche_patient = search_patient_by_id(id_cherche)
+print(fiche_patient)
