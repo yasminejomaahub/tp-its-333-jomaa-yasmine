916aec30d29c6b1adea02ad32f5e8d2c17da9f7b
Yasmine JOMAA <yasmine.jomaa@etu.u-pec.fr>
2026-01-13 09:16:34 +0100

    project 2 completed

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,27 +2,32 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
+#@app.route('/api/salutation', methods=['GET'])
+#def salutation():
+#    return jsonify({
+#        "message": "Hello World"
+#    })
+
+### EXO2 - API with simple display
 #@app.route('/')
 #def index():
 #    message = "Hello world"
 #    return render_template('index.html', message=message)
     
 
-### EXO2 - API with simple display
+### EXO3 - API with parameters display 
+
 #@app.route('/')
 #def index():
 #    user={'name':'john','surname':'doe'}
 #    return render_template('index.html',title='MDM',utilisateur=user)
 
-
-### EXO3 - API with parameters display 
-
 ### EXO4 - API with parameters retrieved from URL 
-@app.route('/')
-def index():
-    # récupère les paramètres 'name' et 'surname' depuis l'URL
-    name = request.args.get('name', 'John')      # valeur par défaut 'John'
-    surname = request.args.get('surname', 'Doe') # valeur par défaut 'Doe'
-    user = {'name': name, 'surname': surname}
-
-    return render_template('index.html', title='MDM', utilisateur=user)
+#@app.route('/')
+#def index():
+#    # récupère les paramètres 'name' et 'surname' depuis l'URL
+#    name = request.args.get('name', 'John')      # valeur par défaut 'John'
+#    surname = request.args.get('surname', 'Doe') # valeur par défaut 'Doe'
+#    user = {'name': name, 'surname': surname}
+#
+#    return render_template('index.html', title='MDM', utilisateur=user)
